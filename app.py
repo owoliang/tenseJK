a813666c2c8a27c09b3b60b67b145d55e996b0cf
owoliang <liangchen1616@gmail.com>
2025-12-28 00:42:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,4 +18,5 @@ def get_node():
     return jsonify(STORY[node_id])
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
